69330f804be914849068c1389f7c69480ab71986
iamrraj <rajr97333@gmail.com>
2022-05-01 22:23:35 +0200

    Updated readme and footer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./Theme/ThemeContext";
 import Background from "./Theme/Background";
-import Toggle from "./Theme";
+
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 const root = ReactDOM.createRoot(document.getElementById("root"));
